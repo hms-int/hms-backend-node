220b4906ae485c0cda167ba143a9455bb222489c
rohit <153482419+mr-saadhak@users.noreply.github.com>
2025-12-25 20:41:38 +0530

    fix: prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,21 @@ module.exports = {
   singleQuote: false,
   trailingComma: "all",
   tabWidth: 2,
+  printWidth: 90,
+  bracketSpacing: true,
+  arrowParens: "always",
+  quoteProps: "consistent",
+  endOfLine: "lf", 
+};
+
+//Previous confis:
+
+/*module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
   printWidth: 100,
   bracketSpacing: true,
   arrowParens: "avoid",
-};
+};*/
