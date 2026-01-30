253dfbb6b499bef0f4c3aa37a15eb06d23e6abaf
rohit <153482419+mr-saadhak@users.noreply.github.com>
2026-01-30 20:55:48 +0530

    refactor: reception service layer migrate start with first function

--- src/services/reception.service.js
+++ src/services/reception.service.js
@@ -1,4 +1,19 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
 
-const 
+const creatReceptionist = async (data) => {
+    const password = data.password || 'reception@123';
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const receptionist = new User({
+        ...data,
+        password: hashedPassword,
+        role: 'receptionist'
+    });
+
+    return await receptionist.save();
+};
+
+const changePassword = async (userId, oldPassword, newPassword) => {
+    const receptionist = await User.findById(userId)
+}
